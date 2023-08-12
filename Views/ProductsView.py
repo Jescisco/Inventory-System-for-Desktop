3d54c5b4160170de7422367ed750543df89e4184
Jesus Delgado <jesusdelgado18.work@gmail.com>
2023-08-12 01:29:25 -0400

    feat and fix page

--- Views/ProductsView.py
+++ Views/ProductsView.py
@@ -1,10 +1,11 @@
 from customtkinter import *
 from tkinter import ttk
+from Resources.Custom_window import CustomToplevel
 from Controllers.ProductsController import ProductsController
 cp = '#0D55B4'
 cs= '#91C1FF'
 
-class ProductsView():
+class ProductsView(CustomToplevel):
 
     def __init__(self, app):
         self.app=app
@@ -13,16 +14,13 @@ class ProductsView():
 
     def products(self):
 
-        # container = CTkFrame(self.app)
-        # container.pack()
-
         title=CTkLabel(self.app, text="Productos", bg_color=cp, width=974, height=55, font=("", 40))
         title.pack()
 
         miniContainer = CTkFrame(self.app, width=20, height=50)
-        miniContainer.pack(side=LEFT, padx=20)
+        miniContainer.pack(side=LEFT, padx=30)
 
-        btnRegister = CTkButton(miniContainer, text="Registrar", width=150, height=50)
+        btnRegister = CTkButton(miniContainer, text="Registrar", command=self.form, width=150, height=50)
         btnRegister.pack(pady=10)
         btnEdit = CTkButton(miniContainer, text="Editar", width=150, height=50)
         btnEdit.pack()
@@ -38,11 +36,11 @@ class ProductsView():
         self.grid.place(x=275, y=120, width=600, height=380)
 
         self.grid.column("#0", width=0, anchor=CENTER)
-        self.grid.column("#1",width=30, anchor=CENTER)
-        self.grid.column("#2",width=45, anchor=CENTER)
-        self.grid.column("#3",width=35, anchor=CENTER)
-        self.grid.column("#4",width=35, anchor=CENTER)
-        self.grid.column("#5",width=45, anchor=CENTER)
+        self.grid.column("#1",width=50, anchor=CENTER)
+        self.grid.column("#2",width=35, anchor=CENTER)
+        self.grid.column("#3",width=30, anchor=CENTER)
+        self.grid.column("#4",width=30, anchor=CENTER)
+        self.grid.column("#5",width=25, anchor=CENTER)
 
         self.grid.heading("#0", text="id", anchor=CENTER)
         self.grid.heading("#1", text="Nombre", anchor=CENTER)
@@ -64,5 +62,21 @@ class ProductsView():
             self.grid.insert('', i, text=product[0], values=(product[1],product[2],product[3],product[4],product[5]))
 
     def form(self):
-        pass
+        x= CustomToplevel(self.app)
+        x.geometry("274x350")
+        title = CTkLabel(x, text="Registrar", bg_color=cp, width=274, height=35, font=("", 20))
+        title.pack()
+        entryName = CTkEntry(x, placeholder_text='Nombre', width=140, height=40)
+        entryName.pack(pady=8)
+        entryPP = CTkEntry(x, placeholder_text='Precio de Compra', width=140, height=40)
+        entryPP.pack()
+        entryPV = CTkEntry(x, placeholder_text='Precio de Venta', width=140, height=40)
+        entryPV.pack(pady=8)
+        amount = CTkEntry(x, placeholder_text='Existencia', width=140, height=40)
+        amount.pack()
+        code = CTkEntry(x, placeholder_text='Codigo', width=140, height=40)
+        code.pack(pady=8)
+        btnSend = CTkButton(x, text='Enviar', width=140, height=40)
+        btnSend.pack(pady=10)
+        
 
