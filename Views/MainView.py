51381a81b494f14c70037a5b7bdbd82ec6158777
Da30Rax <danielramirez1049@gmail.com>
2023-08-11 23:36:42 -0400

    xd

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,19 +1,29 @@
 from customtkinter import *
 cp = '#0D55B4'
+
 class MainView():
+
     def __init__(self, app):
         self.app = app
 
-        title = CTkLabel(self.app, text="Inventario", bg_color=cp, width=574, height=55, font=("", 40))
+        title=CTkLabel(self.app, text="Inventario", bg_color=cp, width=574, height=55, font=("", 40))
         title.pack()
 
-        frame = CTkFrame(self.app, bg_color='transparent', height=500, width=400)
-        frame.pack(pady=70)
+        container=CTkFrame(self.app, bg_color='transparent', height=500, width=400)
+        container.pack(pady=70)
 
-        btn1 = CTkButton(frame, text='Productos', height=50, width= 150)
-        btn1.pack(pady=10)
-        btn2 = CTkButton(frame, text='Ventas', height=50, width= 150)
-        btn2.pack(pady=10)
+        products_button=CTkButton(container, text='Productos', height=50, width= 150, command=lambda:self.products_view(container))
+        products_button.pack(pady=10)
 
+        sales_button=CTkButton(container, text='Ventas', height=50, width= 150)
+        sales_button.pack(pady=10)
 
-        
+    def products_view(self, container):
+        from Views.ProductsView import ProductsView
+        self.clean_cont(container)
+        products_view=ProductsView(self.app)
+        products_view.products(container)
+
+    def clean_cont(self, container):
+        for widget in container.winfo_children():
+            widget.destroy()
---
+++ Views/ProductsView.py
@@ -0,0 +1,11 @@
+from customtkinter import *
+cp = '#0D55B4'
+
+class ProductsView():
+
+    def __init__(self, app):
+        self.app = app
+        self.app.geometry("975x550")
+
+    def products(self, container):
+        pass
