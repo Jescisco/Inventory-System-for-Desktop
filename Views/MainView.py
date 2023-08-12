5a9c37e92cd7a5a557afb6e5bba2606777489337
Jesus Delgado <jesusdelgado18.work@gmail.com>
2023-08-11 23:30:03 -0400

    feat page

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,3 +1,19 @@
-
+from customtkinter import *
+cp = '#0D55B4'
 class MainView():
-    pass
+    def __init__(self, app):
+        self.app = app
+
+        title = CTkLabel(self.app, text="Inventario", bg_color=cp, width=574, height=55, font=("", 40))
+        title.pack()
+
+        frame = CTkFrame(self.app, bg_color='transparent', height=500, width=400)
+        frame.pack(pady=70)
+
+        btn1 = CTkButton(frame, text='Productos', height=50, width= 150)
+        btn1.pack(pady=10)
+        btn2 = CTkButton(frame, text='Ventas', height=50, width= 150)
+        btn2.pack(pady=10)
+
+
+        
---
+++ Views/Products.py
@@ -0,0 +1,8 @@
+from customtkinter import *
+cp = '#0D55B4'
+class Products():
+    def __init__(self, app):
+        self.app = app
+        self.app.geometry("975x550")
+
+        
