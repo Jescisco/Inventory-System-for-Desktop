db8e2317df36a693f4cb708d8176dbccc27746d5
Da30Rax <danielramirez1049@gmail.com>
2023-08-13 19:16:57 -0400

    xd

--- Resources/Includes/Querys.py
+++ Resources/Includes/Querys.py
@@ -17,6 +17,6 @@ SALES_QUERYS={
 }
 
 STORE_QUERYS={
-    "read_sales":"SELECT p.name,s.code,s.lot,s.sale_price,s.date FROM products as p INNER JOIN sales as s ON p.code=s.code ORDER BY p.name,s.date ASC",
-    "read_sales_of_the_day":"SELECT p.name,s.code,s.lot,s.sale_price,s.date FROM products as p INNER JOIN sales as s ON p.code=s.code WHERE s.date=? ORDER BY p.name ASC"
+    "read_sales":"SELECT s.id,p.name,s.code,s.lot,s.sale_price,s.date FROM products as p INNER JOIN sales as s ON p.code=s.code ORDER BY p.name,s.date ASC",
+    "read_sales_of_the_day":"SELECT s.id,p.name,s.code,s.lot,s.sale_price,s.date FROM products as p INNER JOIN sales as s ON p.code=s.code WHERE s.date=? ORDER BY p.name ASC"
 }
